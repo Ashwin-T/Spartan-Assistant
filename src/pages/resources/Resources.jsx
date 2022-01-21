2e39e4fa4904c519729094457516f1677a99393d
Taiga Narusawa <taiganarusawa@gmail.com>
2022-01-21 14:33:30 -0800

    Resources

--- src/pages/resources/Resources.jsx
+++ src/pages/resources/Resources.jsx
@@ -1,3 +1,5 @@
+import  './resources.css'
+
 const Resources = () => {
 
     const links = [
@@ -18,19 +20,26 @@ const Resources = () => {
     return(
         <>
         <div className="flexbox column center">
-            <div className="flexbox column center container">
+            <div className="flexbox column container">
                 <h1>Resources</h1>
 
-                <h3>Links</h3>
-                <div className="flexbox left">
-                    {links.map((link, index) => {
-                        return (
-                            <a key = {index} className="flexbox align-items-right importantLinks" href={link.link} target="_blank" rel="noopener noreferrer">
-                                <h3>{link.name}</h3>
-                            </a>
-                        )
-                    })}
+                <div className="flexbox row center">
+                    <div className="flexbox faq-container">
+                        
+                    </div>
+                    <div className="flexbox link-container">
+                        <h3 style={{textAlign: 'center', width: 100+"%"}}>Links</h3>
+                        {links.map((link, index) => {
+                            return (
+                                <a key = {index} className="flexbox align-items-right importantLinks" href={link.link} target="_blank" rel="noopener noreferrer">
+                                    <h3>{link.name}</h3>
+                                </a>
+                            )
+                        })}
+                    </div>
+                    
                 </div>
+                
             </div>
         </div>
         </>
