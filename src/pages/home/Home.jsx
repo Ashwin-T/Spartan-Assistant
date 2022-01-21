cd08acfe5325b6cb01532fc04c33812904aa0570
Ashwin Talwalkar <atalwalkar719@gmail.com>
2022-01-21 14:40:42 -0800

    changes to links

--- src/pages/home/Home.jsx
+++ src/pages/home/Home.jsx
@@ -9,6 +9,7 @@ import {FaDirections} from 'react-icons/fa';
 import {Link} from 'react-router-dom';
 import {BsFillPeopleFill} from 'react-icons/bs';
 import {FiSettings} from 'react-icons/fi';
+import {ImHangouts} from 'react-icons/im';
 const Home = () => {
 
     const auth = getAuth(app);
@@ -38,19 +39,26 @@ const Home = () => {
                     <h1>Welcome {user.displayName.split(" ")[0]}!</h1>
 
                     <h3>Today is: {today}</h3>
-                    <div className = 'flexbox center'>
-                            <Link className = 'flexbox center importantLinks' to = {{pathname: '/map'}}>
-                                <FaDirections style = {{color: 'dodgerblue'}} size = {35}/>
+                    <div className = 'flexbox column center'>
+                            <Link className = 'flexbox center homeLinks' to = {{pathname: '/map'}}>
+                                <FaDirections style = {{color: 'dodgerblue'}} size = {30}/>
                                 <h3>Find Your Classes</h3>
                             </Link>
-                            <Link className = 'flexbox center importantLinks' to = {{pathname: '/resources'}}>
-                                <BsFillPeopleFill style = {{color: 'green'}}size = {35}/>
+                            <Link className = 'flexbox center homeLinks' to = {{pathname: '/resources'}}>
+                                <BsFillPeopleFill style = {{color: '#D7BE69'}}size = {30}/>
                                 <h3>Resources to Help</h3>
                             </Link>
-                            <Link className = 'flexbox center importantLinks' to = {{pathname: '/settings'}}>
-                                <FiSettings style = {{color: 'grey'}}size = {35}/>
-                                <h3>Update Your Settings</h3>
+                            <Link className = 'flexbox center homeLinks' to = {{pathname: '/settings'}}>
+                                <FiSettings style = {{color: 'grey'}}size = {30}/>
+                                <h3>Update Settings</h3>
                             </Link>
+                            {
+                                localStorage.getItem('freshmen') === 'true' && 
+                                <Link className = 'flexbox center importantLinks' to = {{pathname: '/settings'}}>
+                                    <ImHangouts style = {{color: 'green'}}size = {30}/>
+                                    <h3>Chat With Your Pod</h3>
+                            </Link>
+                            }
                     </div>
                 </div>
             : <Loading />}
