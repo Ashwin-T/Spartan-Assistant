5c971aef6328d62d425e5decf48bacad788bf5d1
Wyatt Cowley <wycowley@gmail.com>
2022-01-25 21:55:07 -0800

    style revamps

--- src/pages/home/Home.jsx
+++ src/pages/home/Home.jsx
@@ -1,80 +1,100 @@
 import { useAuthState } from "react-firebase-hooks/auth";
 import { getAuth } from "@firebase/auth";
-import app from '../../tools/Firebase'
-import {useEffect, useState} from 'react'
-import moment from 'moment';
-import Loading from '../../components/loading/Loading';
-import './home.css';
-import {FaDirections} from 'react-icons/fa';
-import {Link} from 'react-router-dom';
-import {BsFillPeopleFill} from 'react-icons/bs';
-import {FiSettings} from 'react-icons/fi';
-import {ImHangouts} from 'react-icons/im';
+import app from "../../tools/Firebase";
+import { useEffect, useState } from "react";
+import moment from "moment";
+import Loading from "../../components/loading/Loading";
+import "./home.css";
+import { FaDirections } from "react-icons/fa";
+import { Link } from "react-router-dom";
+import { BsFillPeopleFill } from "react-icons/bs";
+import { FiSettings } from "react-icons/fi";
+import { ImHangouts } from "react-icons/im";
 const Home = () => {
-
     const auth = getAuth(app);
     const [user] = useAuthState(auth);
 
-    const [today, setToday] = useState(moment().format('MMMM Do YYYY, h:mm:ss a'));
+    const [today, setToday] = useState(moment().format("MMMM Do YYYY, h:mm:ss a"));
 
     const [loading, setLoading] = useState(false);
-    const [linkStyle, setLinkStyle] = useState()
+    const [linkStyle, setLinkStyle] = useState();
 
     useEffect(() => {
-
         setLoading(true);
 
-        setToday(moment().format('dddd') + " " + moment().format("MMM Do"));
-        
-        if(window.innerWidth > 786){
-            setLinkStyle('wrap')
-        }
-        else{
-            setLinkStyle('')
+        setToday(moment().format("dddd") + " " + moment().format("MMM Do"));
+
+        if (window.innerWidth > 786) {
+            setLinkStyle("wrap");
+        } else {
+            setLinkStyle("");
         }
 
         setLoading(false);
-    }, [])
-
-
+    }, []);
 
     return (
         <>
-        <div className="flexbox column center">
-            {!loading ?
-                <div className="flexbox column center container">
-                    <h1>Welcome {user.displayName.split(" ")[0]}!</h1>
+            <div>
+                {!loading ? (
+                    <div>
+                        <div className='flexbox row full-size'>
+                            <div className='welcome-container'>
+                                <h1>Welcome, {user.displayName.split(" ")[0]}.</h1>
+                                <h3>Have a great {today.split(" ")[0]}</h3>
+                            </div>
+                            <div className='right-triangle'></div>
+                            <div className='action-container'>
+                                <h3>Actions</h3>
 
-                    <h3>Today is: {today}</h3>
-                    <div className = {'links flexbox column center ' + linkStyle} >
-                            <Link className = 'flexbox center homeLinks' to = {{pathname: '/map'}}>
-                                <FaDirections style = {{color: 'dodgerblue'}} size = {30}/>
-                                <h3>Find Your Classes</h3>
-                            </Link>
-                            <Link className = 'flexbox center homeLinks' to = {{pathname: '/resources'}}>
-                                <BsFillPeopleFill style = {{color: '#D7BE69'}}size = {30}/>
-                                <h3>Resources to Help</h3>
-                            </Link>
-                            <Link className = 'flexbox center homeLinks' to = {{pathname: '/settings'}}>
-                                <FiSettings style = {{color: 'grey'}}size = {30}/>
-                                <h3>Update Settings</h3>
-                            </Link>
-                            {
-                                localStorage.getItem('freshmen') === 'true' && 
-                                <Link className = 'flexbox center homeLinks' to = {{pathname: '/settings'}}>
-                                    <ImHangouts style = {{color: 'green'}}size = {30}/>
-                                    <h3>Chat With Your Pod</h3>
-                            </Link>
-                            }
+                                <div className={"links flexbox column center " + linkStyle}>
+                                    <Link className='flexbox homeLinks' to={{ pathname: "/map" }}>
+                                        <FaDirections style={{ color: "dodgerblue" }} size={30} />
+                                        <h3>Find Your Classes</h3>
+                                    </Link>
+                                    <Link className='flexbox homeLinks' to={{ pathname: "/resources" }}>
+                                        <BsFillPeopleFill style={{ color: "#D7BE69" }} size={30} />
+                                        <h3>Resources to Help</h3>
+                                    </Link>
+                                    <Link className='flexbox homeLinks' to={{ pathname: "/settings" }}>
+                                        <FiSettings style={{ color: "grey" }} size={30} />
+                                        <h3>Update Settings</h3>
+                                    </Link>
+                                </div>
+                            </div>
+                        </div>
                     </div>
-                </div>
-            : <Loading />}
-        </div>
+                ) : (
+                    // <div className='flexbox column center container'>
+                    //     <h1>Welcome, {user.displayName.split(" ")[0]}.</h1>
+
+                    //     <h3>Today is {today}</h3>
+                    // <div className={"links flexbox column center " + linkStyle}>
+                    //     <Link className='flexbox center homeLinks' to={{ pathname: "/map" }}>
+                    //         <FaDirections style={{ color: "dodgerblue" }} size={30} />
+                    //         <h3>Find Your Classes</h3>
+                    //     </Link>
+                    //     <Link className='flexbox center homeLinks' to={{ pathname: "/resources" }}>
+                    //         <BsFillPeopleFill style={{ color: "#D7BE69" }} size={30} />
+                    //         <h3>Resources to Help</h3>
+                    //     </Link>
+                    //     <Link className='flexbox center homeLinks' to={{ pathname: "/settings" }}>
+                    //         <FiSettings style={{ color: "grey" }} size={30} />
+                    //         <h3>Update Settings</h3>
+                    //     </Link>
+                    //         {localStorage.getItem("freshmen") === "true" && (
+                    //             <Link className='flexbox center homeLinks' to={{ pathname: "/settings" }}>
+                    //                 <ImHangouts style={{ color: "green" }} size={30} />
+                    //                 <h3>Chat With Your Pod</h3>
+                    //             </Link>
+                    //         )}
+                    //     </div>
+                    // </div>
+                    <Loading />
+                )}
+            </div>
         </>
     );
-}
-
+};
 
 export default Home;
-
- 
