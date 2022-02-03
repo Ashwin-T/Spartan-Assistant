43e95c3c532770dcb0fa0bbfe14079fe5d45e33a
Ashwin Talwalkar <atalwalkar719@gmail.com>
2022-02-02 21:45:53 -0800

    jackson does mpre css

--- src/pages/map/StaticMap.jsx
+++ src/pages/map/StaticMap.jsx
@@ -1,7 +1,10 @@
-
+import {MdOutlineArrowBackIos} from 'react-icons/md'
 const StaticMap  = ()=> {
     return (
         <>  
+            <div className="flexbox back-arrow-wrapper">
+                <MdOutlineArrowBackIos size = {40} onClick={()=>{window.history.back()}}/>   
+            </div>
             <div className="static-map flexbox column center">
                 <img src = 'images/staticmap.svg' alt="map"/>
             </div>
