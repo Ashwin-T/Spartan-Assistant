180f0a72189d536042b40a001d814052557130a5
Wyatt Cowley <wycowley@gmail.com>
2022-02-03 10:10:37 -0800

    making look cool

--- src/App.jsx
+++ src/App.jsx
@@ -1,68 +1,70 @@
-import React, {useState, useEffect} from 'react';
-import { getAuth } from "firebase/auth"
-import { useAuthState } from 'react-firebase-hooks/auth';
+import React, { useState, useEffect } from "react";
+import { getAuth } from "firebase/auth";
+import { useAuthState } from "react-firebase-hooks/auth";
 
-import Login from './pages/login/Login';
-import Error from './components/error/Error';
-import Loading from './components/loading/Loading';
-import Source from './pages/Source';
-import app from './tools/Firebase'
-import { motion } from "framer-motion/dist/framer-motion"
+import Login from "./pages/login/Login";
+import Error from "./components/error/Error";
+import Loading from "./components/loading/Loading";
+import Source from "./pages/Source";
+import app from "./tools/Firebase";
+import { motion } from "framer-motion/dist/framer-motion";
 
+const App = () => {
+    const auth = getAuth(app);
+    const [user, loading, error] = useAuthState(auth);
+    // auth.signOut();
 
-const App = () =>{
+    const [allow, setAllow] = useState(false);
 
-  const auth = getAuth(app);
-  const [user, loading, error] = useAuthState(auth);
-  // auth.signOut();
+    useEffect(() => {
+        if (localStorage.getItem("allow") === "true") {
+            setAllow(true);
+        } else {
+            const timeout = setTimeout(() => {
+                setAllow(true);
+            }, 3000);
 
-  const [allow, setAllow] = useState(false);
+            return () => {
+                clearTimeout(timeout);
+            };
+        }
+    }, []);
 
-  useEffect(() => {
-    if(localStorage.getItem('allow') === 'true'){
-      setAllow(true);
-    } 
-    else{
-      const timeout = setTimeout(() => {
-        setAllow(true);
-      }, 3000)
+    //window.localStorage.clear(); // for testing
 
-      return () => {clearTimeout(timeout)}
-    }
-    
-  } , [])
+    const imgVariants = {
+        hidden: {
+            opacity: 0,
+        },
+        visible: {
+            transition: {
+                duration: 0.5,
+            },
+            opacity: 1,
+        },
+    };
 
-  //window.localStorage.clear(); // for testing
-
-
-  const imgVariants = {
-    hidden: {
-      opacity: 0,
-    },
-    visible: {
-      transition: {
-        duration: 0.5,
-      },
-      opacity: 1,
-    }
-  }
-  
-  return (
-    <>
-      {!loading ? !error ? !allow ? 
-      
-        <div className= 'onLoad flexbox column center'>
-          <motion.img 
-          src={'images/logo.png'} alt="logo"
-          variants={imgVariants}
-          initial="hidden"
-          animate="visible"
-          />
-        </div> 
-        
-        : user ? <Source/> : <Login />: <Error /> : <Loading />}
-    </>
-  );
-}
+    return (
+        <>
+            {!loading ? (
+                !error ? (
+                    !allow ? (
+                        <div className='onLoad flexbox column center'>
+                            <motion.img src={"images/logo.png"} alt='logo' variants={imgVariants} initial='hidden' animate='visible' />
+                        </div>
+                    ) : user ? (
+                        <Source />
+                    ) : (
+                        <Login />
+                    )
+                ) : (
+                    <Error />
+                )
+            ) : (
+                <Loading />
+            )}
+        </>
+    );
+};
 
 export default App;
--- src/pages/resources/Resources.jsx
+++ src/pages/resources/Resources.jsx
@@ -1,91 +1,82 @@
-import  './resources.css'
-import * as faqs from '../../data/Faq.json';
-import Accordion from '@mui/material/Accordion';
-import AccordionSummary from '@mui/material/AccordionSummary';
-import AccordionDetails from '@mui/material/AccordionDetails';
+import "./resources.css";
+import * as faqs from "../../data/Faq.json";
+import Accordion from "@mui/material/Accordion";
+import AccordionSummary from "@mui/material/AccordionSummary";
+import AccordionDetails from "@mui/material/AccordionDetails";
 // import Typography from '@mui/material/Typography';
 // import ExpandMoreIcon from '@mui/icons-material/ExpandMore';
-import {AiOutlineArrowDown} from 'react-icons/ai';
+import { AiOutlineArrowDown } from "react-icons/ai";
 
 const Resources = () => {
-
     const links = [
         {
-            name: 'Canvas',
-            link: 'https://mvla.instructure.com/',
-            description: 'Canvas is a web application that allows teachers to create and manage course content.',
-            image: 'images/canvasLogo.png'
+            name: "Canvas",
+            link: "https://mvla.instructure.com/",
+            description: "Canvas is a web application that allows teachers to create and manage course content.",
+            image: "images/canvasLogo.png",
         },
         {
-            name: 'Aeries',
-            link: 'https://mvla.asp.aeries.net/student/LoginParent.aspx?page=default.aspx',
-            description: 'Aeries is a school management and grading system for students and parents.',
-            image: 'images/aeriesLogo.png'
+            name: "Aeries",
+            link: "https://mvla.asp.aeries.net/student/LoginParent.aspx?page=default.aspx",
+            description: "Aeries is a school management and grading system for students and parents.",
+            image: "images/aeriesLogo.png",
         },
         {
-            name: 'Google Classroom',
-            link: 'https://classroom.google.com',
-            description: 'Google Classroom is a free online course management tool for teachers and students.',
-            image: 'images/google-classroom-logo.png'
+            name: "Google Classroom",
+            link: "https://classroom.google.com",
+            description: "Google Classroom is a free online course management tool for teachers and students.",
+            image: "images/google-classroom-logo.png",
         },
         {
-            name: 'Periods.io',
-            link: 'https://periods.io',
-            description: 'Manage your time more effectively by tracking the remaining time in class',
-            image: 'images/periodsioLogo.png'
+            name: "Periods.io",
+            link: "https://periods.io",
+            description: "Manage your time more effectively by tracking the remaining time in class",
+            image: "images/periodsioLogo.png",
         },
         {
-            name: 'Mvhs.io',
-            link: 'https://mvhs.io',
-            description: 'Mvhs.io is a versitle app for MVHS staff and students.',
-            image: 'images/mvhsioLogo.png'
+            name: "Mvhs.io",
+            link: "https://mvhs.io",
+            description: "Mvhs.io is a versitle app for MVHS staff and students.",
+            image: "images/mvhsioLogo.png",
         },
+    ];
 
-    ]
-    
-    return(
+    return (
         <>
-        <div className="flexbox column center">
-                <h1 className="titleResources">Resources</h1>
+            <div className='flexbox column helloWorld'>
+                <h1 className='titleResources'>Resources</h1>
 
-                <div className="flexbox row center">
-                    <div className="flexbox faq-container">
+                <div className='flexbox row'>
+                    <div className='flexbox faq-container'>
                         <h3>FAQ</h3>
                         {faqs.faqs.map((item, index) => {
-                            return(
+                            return (
                                 <Accordion>
-                                <AccordionSummary
-                                expandIcon={<AiOutlineArrowDown />}
-                                aria-controls="panel1a-content"
-                                id="panel1a-header"
-                                >
-                                <h3>{item.faq}</h3>
-                                </AccordionSummary>
-                                <AccordionDetails>
-                                <h4>{item.answer}</h4>
-                                </AccordionDetails>
-                            </Accordion>
-                            )
+                                    <AccordionSummary expandIcon={<AiOutlineArrowDown />} aria-controls='panel1a-content' id='panel1a-header'>
+                                        <h3>{item.faq}</h3>
+                                    </AccordionSummary>
+                                    <AccordionDetails>
+                                        <p>{item.answer}</p>
+                                    </AccordionDetails>
+                                </Accordion>
+                            );
                         })}
-                    
-                    
                     </div>
-                    <div className="link-container">
-                        <h3 style={{textAlign: 'center', width: 100+"%"}}>Links</h3>
+                    <div className='link-container'>
+                        <h3>Links</h3>
                         {links.map((link, index) => {
                             return (
-                                <a key = {index} className="flexbox importantLinks" href={link.link} target="_blank" rel="noopener noreferrer">
-                                    <img src={link.image} alt = 'logo for icon' />
+                                <a key={index} className='flexbox importantLinks' href={link.link} target='_blank' rel='noopener noreferrer'>
+                                    <img src={link.image} alt='logo for icon' />
                                     <h3>{link.name}</h3>
                                 </a>
-                            )
+                            );
                         })}
                     </div>
-                                    
+                </div>
             </div>
-        </div>
         </>
-    )
-}
+    );
+};
 
 export default Resources;
