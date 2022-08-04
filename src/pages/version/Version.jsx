38eabd76f16d6c3fde92706a9d036d692196ab28
Ashwin Talwalkar <atalwalkar719@gmail.com>
2022-08-03 18:15:07 -0700

    version

--- src/pages/version/Version.jsx
+++ src/pages/version/Version.jsx
@@ -2,7 +2,7 @@ const Version = () => {
     return (
         <>
             <div className="flexbox column center">
-                <h3>Spartan Assistant was last updated on: 5/25/2022 at 12:22 pm</h3>
+                <h3>Spartan Assistant was last updated on: 8/3/2022 at 6:1 pm</h3>
             </div>
         </>
       );
